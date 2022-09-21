f1dd83a58ae6bf2fd2529c7a404780e6844cdc9d
fraserdscott <fraserdscott@gmail.com>
2022-09-21 23:25:49 +0100

    Fuzz test deposit contract

--- src/Rollup.sol
+++ src/Rollup.sol
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+// TODO replace this with lib import
 import "./MerkleTreeWithHistory.sol";
 
 contract Rollup is MerkleTreeWithHistory {
-    constructor(IHasher hasher) MerkleTreeWithHistory(4, hasher) {}
+    constructor(uint32 levels, address hasher)
+        MerkleTreeWithHistory(levels, IHasher(hasher))
+    {}
 
     function deposit() external payable {
-        bytes32 _leaf = keccak256(abi.encodePacked(msg.sender, msg.value));
+        //  need to hash the inputs into a field element
+        bytes32 _leaf = bytes32(
+            uint256(keccak256(abi.encodePacked(msg.sender, msg.sender))) %
+                FIELD_SIZE
+        );
+
         _insert(_leaf);
     }
 }
